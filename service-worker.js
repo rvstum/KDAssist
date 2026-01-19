2e12b1b5f2780f4778419ccd6772a2e4f8d06a08
rvstum <rvstum@gmail.com>
2026-01-18 17:22:57 -0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kdassist-v1.6.9'; // Change version number to force update
+const CACHE_NAME = 'kdassist-v1.7.3'; // Change version number to force update
 const urlsToCache = [
   './',
   './index.html',
